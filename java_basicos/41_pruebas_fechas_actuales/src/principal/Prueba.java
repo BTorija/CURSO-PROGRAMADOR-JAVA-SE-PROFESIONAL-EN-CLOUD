240814fe540ec388c0270e1850819391940cbf8a
BeaTorija <147420664+BTorija@users.noreply.github.com>
2024-04-05 13:05:28 +0200

    programa 42

---
+++ java_basicos/41_pruebas_fechas_actuales/src/principal/Prueba.java
@@ -0,0 +1,15 @@
+package principal;
+
+import java.time.LocalDate;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		LocalDate f1=LocalDate.now();
+		LocalDate f2=LocalDate.of(2021, 11, 3);
+		System.out.println(f1);
+		System.out.println(f2);
+
+	}
+
+}
