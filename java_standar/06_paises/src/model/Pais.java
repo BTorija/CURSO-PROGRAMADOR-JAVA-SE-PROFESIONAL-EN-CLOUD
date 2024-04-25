a0113f0c1aaa0fecd5c368b3f0f5939d7dfb6310
BeaTorija <147420664+BTorija@users.noreply.github.com>
2024-04-25 13:43:54 +0200

    commit 2

--- java_standar/06_paises/src/model/Pais.java
+++ java_standar/06_paises/src/model/Pais.java
@@ -1,5 +1,22 @@
 package model;
 
+
+@Dat
 public class Pais {
+	/*
+	 * Implementar lógica de negocio de una aplicación que proporcione información
+sobre paises. Cada pais se caracteriza por: nombre,continente,capital,habitantes
+
+-Lista de continentes
+-Lista de paises a partir del continente
+-Pais más poblado
+-Tabla con paises agurpados por continente
+-Pais a partir de su capital 
+	 */
+	private String nombre;
+	private String continente;
+	private String capital;
+	private Long habitantes;
+	
 
 }
